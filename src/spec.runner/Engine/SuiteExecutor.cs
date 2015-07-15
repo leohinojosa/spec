d6f88d0aff953c693afc67464a89c828423f647a
Leonardo Hinojosa <leonardh@gmail.com>
2015-07-15 09:33:33 -0700

    moved runner output to console main

--- src/spec.runner/Engine/SuiteExecutor.cs
+++ src/spec.runner/Engine/SuiteExecutor.cs
@@ -7,19 +7,18 @@ namespace spec.runner.Engine
 {
   public class SuiteExecutor
   {
-    public void Execute(IEnumerable<Registry> sources)
+    public TestSummary Execute(IEnumerable<Registry> sources)
     {
 
       var groups = sources.GroupBy(x => x.Source, x => x, (x, y) => new
       {
         Source = x,
-        TestCases = y.SelectMany(a=>a.runnableLookupTable)
+        TestCases = y.SelectMany(a => a.runnableLookupTable)
       }).ToList();
 
       var results = new List<TestSummary>();
       foreach (var groupedItem in groups)
       {
-       
         using (var sandbox = new Sandbox<Executor>(groupedItem.Source))
         {
           var targetTypes = groupedItem.TestCases.Select(x => x.ClassName.ToLower()).Distinct().ToArray();
@@ -28,18 +27,14 @@ namespace spec.runner.Engine
         }
       }
 
-      var summary = new TestSummary
+      return new TestSummary
       {
         total = results.Sum(x => x.total),
         passed = results.Sum(x => x.passed),
         failed = results.Sum(x => x.failed),
-        pending = results.Sum(x => x.pending)
+        pending = results.Sum(x => x.pending),
+        specs = results.SelectMany(x => x.specs).Select(x => new SpecSummary { Id = x.Id, ExecutionResult = x.ExecutionResult, RanSuccesfully = x.RanSuccesfully}).ToList()
       };
-
-      Console.WriteLine("\n{0} Total {1} Passed {2} Failed {3} Pending\n", summary.total, summary.passed, summary.failed,
-        summary.pending);
-
-
     }
   }
 }
--- src/spec.runner/Program.cs
+++ src/spec.runner/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
+using System.Text;
+using spec.Model;
 using spec.runner.Engine;
 using spec.runner.Model;
 
@@ -31,8 +34,72 @@ namespace spec.runner
       var specRegistries = new Engine.SuiteDiscovery(sources).Discover();
 
       var r = new SuiteExecutor();
-      r.Execute(specRegistries);
+      summary = r.Execute(specRegistries);
+
+      //vamos a recorrer
+      foreach (var spec in specRegistries.SelectMany(x => x.currentDeclarationSuite.Childs))
+      {
+        whitespace = String.Empty;
+        Console.WriteLine("");
+        PrintOut(spec);
+      }
+      Console.WriteLine("\n{0} Total {1} Passed {2} Failed {3} Pending\n", summary.total, summary.passed, summary.failed,
+        summary.pending);
+
       Console.ReadLine();
+    }
+
+    private static TestSummary summary;
+    private static string whitespace = ".";
+    public static void PrintOut(Definition spec)
+    {
+      whitespace = whitespace + ".";
+      if (spec.GetType() == typeof(Specification))
+      {
+        var specResult = summary.specs.SingleOrDefault(x => x.Id == spec.Id);
+        print(specResult.RanSuccesfully, spec.Enabled);
+        Console.WriteLine( spec.Description.Trim());
+      }
+      else
+      {
+        Console.WriteLine(whitespace + spec.Description.Trim());
+      }
+
+      if (spec.Childs.Count > 0)
+      {
+        whitespace = whitespace + ".";
+        foreach (var definition in spec.Childs)
+        {
+          PrintOut(definition);
+        }
+        whitespace = whitespace.TrimEnd('.');
+      }
+      whitespace = whitespace.TrimEnd('.');
+    }
+
+    public static void print(bool ranSuccesful, bool enabled)
+    {
+      var label = String.Empty;
+      if (enabled)
+      {
+        if (ranSuccesful)
+        {
+          Console.ForegroundColor = ConsoleColor.Green;
+          label = "\u221A";
+        }
+        else
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          label = "x";
+        }
+      }
+      else
+      {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        label = "\u25A0";
+      }
+      Console.Write(whitespace + label + " ");
+      Console.ForegroundColor = ConsoleColor.Gray;
     }
   }
 }
