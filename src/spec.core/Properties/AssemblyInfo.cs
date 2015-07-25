dca4127dd016800285afe59f2cf3c2b29b798275
Leonardo Hinojosa <leonardh@gmail.com>
2015-07-24 18:48:10 -0700

    added nuget package details

--- src/spec.core/Properties/AssemblyInfo.cs
+++ src/spec.core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("describe")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("spec")]
+[assembly: AssemblyDescription("A bdd framework to test your code.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tesla Motors")]
-[assembly: AssemblyProduct("describe")]
-[assembly: AssemblyCopyright("Copyright © Tesla Motors 2015")]
+[assembly: AssemblyCompany("Leo Hinojosa")]
+[assembly: AssemblyProduct("spec")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
