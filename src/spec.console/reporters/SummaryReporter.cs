b6944da493b7c883a2ab137c86e67609b0b58d3c
leohinojosa <leonardh@gmail.com>
2015-08-06 18:47:55 -0700

    SummaryReporter All test passed message
    added a message to the All Tests Passed Event

--- src/spec.console/reporters/SummaryReporter.cs
+++ src/spec.console/reporters/SummaryReporter.cs
@@ -18,6 +18,14 @@ namespace spec.console.reporters
         TotalFailed = executionResult.Count(x => x.Enabled && !x.RanSuccesfully),
         TotalPending = executionResult.Count(x => !x.Enabled)
       };
+
+      if (summary.TotalPassed == summary.TotalTests)
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("\n All tests passed! You rock!");
+        Console.ForegroundColor = ConsoleColor.Gray;
+      }
+
       Console.WriteLine("\n {0} Total, {1} Passed, {2} Failed, {3} Pending", summary.TotalTests, summary.TotalPassed,
         summary.TotalFailed, summary.TotalPending);
     }
