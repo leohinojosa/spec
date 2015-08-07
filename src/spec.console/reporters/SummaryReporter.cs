1a8d3a1d1f18b47fbd3b8e3a5d94d764ed31e119
leohinojosa <leonardh@gmail.com>
2015-08-06 18:54:42 -0700

    added  total elapsed seconds to summary reporter
    it now displays how many seconds elapsed on the summary

--- src/spec.console/reporters/SummaryReporter.cs
+++ src/spec.console/reporters/SummaryReporter.cs
@@ -10,13 +10,13 @@ namespace spec.console.reporters
   {
     public void Execute(List<Registry> registries, List<DefinitionSource> executionResult)
     {
-
       var summary = new
       {
         TotalTests = executionResult.Count(),
         TotalPassed = executionResult.Count(x => x.Enabled && x.RanSuccesfully),
         TotalFailed = executionResult.Count(x => x.Enabled && !x.RanSuccesfully),
-        TotalPending = executionResult.Count(x => !x.Enabled)
+        TotalPending = executionResult.Count(x => !x.Enabled),
+        TotalSeconds = executionResult.Where(x => x.Enabled).Sum(x => (x.EndTime - x.StartTime).TotalSeconds)
       };
 
       if (summary.TotalPassed == summary.TotalTests)
@@ -26,8 +26,8 @@ namespace spec.console.reporters
         Console.ForegroundColor = ConsoleColor.Gray;
       }
 
-      Console.WriteLine("\n {0} Total, {1} Passed, {2} Failed, {3} Pending", summary.TotalTests, summary.TotalPassed,
-        summary.TotalFailed, summary.TotalPending);
+      Console.WriteLine("\n {0} Total, {1} Passed, {2} Failed, {3} Pending ({4} secs)", summary.TotalTests, summary.TotalPassed,
+        summary.TotalFailed, summary.TotalPending, summary.TotalSeconds);
     }
   }
 }
