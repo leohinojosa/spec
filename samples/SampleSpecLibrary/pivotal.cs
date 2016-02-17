45082551208ae14ea4f8f9d268e5fb8bfe7c9aae
leohinojosa <leonardh@gmail.com>
2016-02-17 09:58:02 -0800

    Added a sample spec standalone proyect

---
+++ samples/SampleSpecLibrary/pivotal.cs
@@ -0,0 +1,147 @@
+﻿using FluentAssertions;
+using spec;
+using spec.core;
+
+namespace SampleSpecLibrary
+{
+  public class pivotal : Spec
+  {
+    public pivotal()
+    {
+      describe("A suite", () =>
+      {
+        it("contains spec with an expectation", () =>
+        {
+          true.Should().Be(true);
+        });
+      });
+
+      describe("A suite is just a function", () =>
+      {
+        bool a;
+
+        it("and so is a spec", () =>
+        {
+          a = true;
+          a.Should().Be(true);
+        });
+      });
+
+
+      describe("The 'toBe' matcher compares with ===", () =>
+      {
+
+        it("and has a positive case", () =>
+        {
+          true.Should().Be(true);
+        });
+
+        it("and can have a negative case", () =>
+        {
+          false.Should().BeFalse();
+        });
+      });
+
+
+      describe("A spec using a beforeach and aftereach", () =>
+      {
+        var foo = 0;
+        beforeEach(() =>
+        {
+          foo += 1;
+        });
+
+        afterEach(() =>
+        {
+          foo = 0;
+        });
+
+        it("is just a function, so it can contain any code", () =>
+        {
+          foo.Should().Be(1);
+        });
+
+        it("can have more than one expectation", () =>
+        {
+          foo.Should().Be(1);
+        });
+
+      });
+
+        describe("A spec using a beforeach and aftereach", () =>
+        {
+            var foo = 0;
+            beforeAll(() => { foo = 1; });
+
+            afterAll(() => { foo = 0; });
+
+            it("sets the initial value of foo before specs run", ()=> {
+                foo.Should().Be(1);
+                foo += 1;
+            });
+
+            it("does not reset foo between specs", ()=> {
+                foo.Should().Be(2);
+            });
+        });
+
+
+
+        describe("A spec", () =>{
+            int? foo = null;
+
+            beforeEach(()=> {
+                foo = 0;
+                foo += 1;
+            });
+
+            afterEach(()=> {
+                foo = 0;
+            });
+
+            it("is just a function, so it can contain any code", () => {
+                foo.Should().Be(1);
+            });
+
+            it("can have more than one expectation", ()=> {
+                foo.Should().Be(1);
+                true.Should().BeTrue();
+            });
+
+            describe("nested inside a second describe", ()=> {
+                int? bar = null;
+
+                beforeEach(()=> {
+                    bar = 1;
+                });
+
+                it("can reference both scopes as needed", ()=> {
+                    foo.Should().Be(bar);
+                });
+            });
+        });
+
+        xdescribe("A spec", () => {
+            int? foo = null;
+
+            beforeEach(() => {
+                foo = 0;
+                foo += 1;
+            });
+
+            it("is just a function, so it can contain any code", () => {
+                foo.Should().Be(1);
+            });
+        });
+        
+        describe("Pending specs", () =>
+        {
+            xit("can be declared 'xit'", () =>{
+                true.Should().Be(false);
+            });
+
+            it("can be declared with 'it' but without a function");
+        });
+    }
+  }
+}
---
+++ samples/SampleSpecLibrary/tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spec.core;
+
+namespace SampleSpecLibrary
+{
+    public class tests : Spec
+    {
+        public tests()
+        {
+            describe("This is a sample spec", () =>
+            {
+                it("Should test for a true value", () =>
+                {
+                    Assert.IsTrue(true);
+                });
+
+                it("Should not really test anything");
+
+                it("run a validation", ()=>
+                {
+                    Assert.IsFalse(true);
+                });
+            });
+        }
+    }
+}
