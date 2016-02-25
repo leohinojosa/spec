3b60de4447d7c2c3fa1eaa9abe42b3ac33dfd259
leohinojosa <leonardh@gmail.com>
2016-02-25 11:28:13 -0800

    reformated code file

--- src/spec.tests/AgentTests.cs
+++ src/spec.tests/AgentTests.cs
@@ -8,63 +8,59 @@ using spec.core.Model;
 
 namespace spec.tests
 {
-  [TestClass]
-  public class AgentTests
-  {
-    private spec_double _suite;
-
-    [TestInitialize]
-    public void Setup()
+    [TestClass]
+    public class AgentTests
     {
-      _suite = new spec_double();
-    }
+        private spec_double _suite;
 
-    //[TestMethod] - Nested describes
-    public void spec_shouldNotAllowAddingNestedSuites()
-    {
-      _suite = new spec_double();
-      _suite.describe("new Describe", () =>
-      {
-          _suite.it("spec", () =>
-          {
-              _suite.describe("invalid", () => { });
-          });
-      });
+        [TestInitialize]
+        public void Setup()
+        {
+            _suite = new spec_double();
+        }
 
-      Agent agent = new Agent();
-      agent.RunSuite(_suite);
-      _suite.Registry.ExecutableLookupTable.Any(x => x.RanSuccesfully)
-        .Should()
-        .BeFalse("describes should not be in specs");
-    }
+        //[TestMethod] - Nested describes
+        public void spec_shouldNotAllowAddingNestedSuites()
+        {
+            _suite = new spec_double();
+            _suite.describe("new Describe",
+                () => { _suite.it("spec", () => { _suite.describe("invalid", () => { }); }); });
 
-    [TestMethod]
-    public void agent_executesASpecAndRunsSuccesfully()
-    {
-      var i = 0;
-      _suite.describe("new", () =>
-      {
-        i = 0;
-        _suite.beforeAll(() => { i++; });
+            Agent agent = new Agent();
+            agent.RunSuite(_suite);
+            _suite.Registry.ExecutableLookupTable.Any(x => x.RanSuccesfully)
+                .Should()
+                .BeFalse("describes should not be in specs");
+        }
 
-        _suite.beforeEach(() => { i++; });
-
-        _suite.it("spec1", () => { i++; });
-
-        _suite.it("spec2", () => { i++; });
-
-        _suite.afterEach(() => { i++; });
-
-        _suite.afterAll(() => { i++; });
-      });
-
-      var agent = new Agent();
-      agent.RunSuite(_suite);
-      i.Should().Be(8);
-    }
         [TestMethod]
-      public void agent_executesASpecHooksEvenAfterErrors()
-      {
+        public void agent_executesASpecAndRunsSuccesfully()
+        {
+            var i = 0;
+            _suite.describe("new", () =>
+            {
+                i = 0;
+                _suite.beforeAll(() => { i++; });
+
+                _suite.beforeEach(() => { i++; });
+
+                _suite.it("spec1", () => { i++; });
+
+                _suite.it("spec2", () => { i++; });
+
+                _suite.afterEach(() => { i++; });
+
+                _suite.afterAll(() => { i++; });
+            });
+
+            var agent = new Agent();
+            agent.RunSuite(_suite);
+            i.Should().Be(8);
+        }
+
+        [TestMethod]
+        public void agent_executesASpecHooksEvenAfterErrors()
+        {
             int i = 0,
                 j = 0;
             _suite.describe("new", () =>
@@ -72,11 +68,20 @@ namespace spec.tests
                 i = 0;
                 _suite.beforeAll(() => { i++; });
 
-                _suite.beforeEach(() => { i++; j++;});
+                _suite.beforeEach(() =>
+                {
+                    i++;
+                    j++;
+                });
 
                 _suite.it("spec1", () => { throw new Exception("im breaking the flow"); });
 
-                _suite.afterEach(() => { i++; ; j++; });
+                _suite.afterEach(() =>
+                {
+                    i++;
+                    ;
+                    j++;
+                });
 
                 _suite.afterAll(() => { i++; });
             });
@@ -85,7 +90,7 @@ namespace spec.tests
             agent.RunSuite(_suite);
             i.Should().Be(4);
             j.Should().Be(2);
-      }
+        }
 
         [TestMethod]
         public void agent_executesASpecAnd()
@@ -106,145 +111,141 @@ namespace spec.tests
         }
 
         [TestMethod]
-    public void agent_executesASpecAndRunsSuccesfullyWithHooks()
-    {
-      var i = 0;
-      _suite.describe("new", () =>
-      {
-        _suite.beforeAll(() => { i++; });
-
-        _suite.beforeEach(() => { i++; });
-
-        _suite.beforeEach(() => { i++; });
-
-        _suite.it("spec1", () =>
+        public void agent_executesASpecAndRunsSuccesfullyWithHooks()
         {
-          //
-        });
-
-        _suite.afterEach(() => { i--; });
-
-        _suite.afterEach(() => { i--; });
-
-        _suite.afterAll(() => { i--; });
-      });
-
-      var agent = new Agent();
-      agent.RunSuite(_suite);
-      i.Should().Be(0);
-    }
-
-    [TestMethod]
-    public void agent_executesASpecAndRunsSuccesfullyWithHooksSiblingDescribes()
-    {
-      var i = 0;
-      _suite.describe("new", () =>
-      {
-          _suite.beforeAll(() => { i++; });
-
-          _suite.beforeEach(() => { i++; });
-
-          _suite.it("spec1", () =>
-          {
-              //
-          });
-
-          _suite.afterEach(() => { i--; });
-
-          _suite.afterAll(() => { i--; });
-      });
-      _suite.describe("new1", () =>
-      {
-          _suite.beforeAll(() => { i++; });
-
-          _suite.beforeEach(() => { i++; });
-
-          _suite.it("spec1", () =>
-          {
-              //
-          });
-
-          _suite.afterEach(() => { i--; });
-
-          _suite.afterAll(() => { i--; });
-      });
-
-      var agent = new Agent();
-      agent.RunSuite(_suite);
-      i.Should().Be(0);
-    }
-
-    [TestMethod]
-    public void agent_executesASpecAndRunsSuccesfullyWithHooksNestedDescribes()
-    {
-      var i = 0;
-      _suite.describe("new", () =>
-      {
-        _suite.describe("Nested1", () =>
-        {
-            _suite.beforeAll(() => { i++; });
-
-            _suite.beforeEach(() => { i++; });
-
-            _suite.it("spec1", () =>
+            var i = 0;
+            _suite.describe("new", () =>
             {
-                //
+                _suite.beforeAll(() => { i++; });
+
+                _suite.beforeEach(() => { i++; });
+
+                _suite.beforeEach(() => { i++; });
+
+                _suite.it("spec1", () =>
+                {
+                    //
+                });
+
+                _suite.afterEach(() => { i--; });
+
+                _suite.afterEach(() => { i--; });
+
+                _suite.afterAll(() => { i--; });
             });
 
-            _suite.afterEach(() => { i--; });
-
-            _suite.afterAll(() => { i--; });
-        });
-        _suite.describe("Nested2", () =>
-        {
-            _suite.beforeAll(() => { i++; });
-
-            _suite.beforeEach(() => { i++; });
-
-            _suite.it("spec1", () =>
-            {
-                //
-            });
-
-            _suite.afterEach(() => { i--; });
-
-            _suite.afterAll(() => { i--; });
-        });
-      });
-
-      var agent = new Agent();
-      agent.RunSuite(_suite);
-      i.Should().Be(0);
-    }
-
-    //[TestMethod] // Dynamic specs have some issues
-    public void suite_shouldAddDYnamicSpecs()
-    {
-      var dynamicSpecCount = 10;
-      _suite.describe("test describe", () =>
-      {
-        for (int i = 0; i < dynamicSpecCount; i++)
-        {
-          _suite.it("the int " + i, () =>
-          {
-            Console.Write(i);
-          });
+            var agent = new Agent();
+            agent.RunSuite(_suite);
+            i.Should().Be(0);
         }
-      });
 
-      _suite.Registry.ExecutableLookupTable.Count.Should().Be(dynamicSpecCount);
+        [TestMethod]
+        public void agent_executesASpecAndRunsSuccesfullyWithHooksSiblingDescribes()
+        {
+            var i = 0;
+            _suite.describe("new", () =>
+            {
+                _suite.beforeAll(() => { i++; });
+
+                _suite.beforeEach(() => { i++; });
+
+                _suite.it("spec1", () =>
+                {
+                    //
+                });
+
+                _suite.afterEach(() => { i--; });
+
+                _suite.afterAll(() => { i--; });
+            });
+            _suite.describe("new1", () =>
+            {
+                _suite.beforeAll(() => { i++; });
+
+                _suite.beforeEach(() => { i++; });
+
+                _suite.it("spec1", () =>
+                {
+                    //
+                });
+
+                _suite.afterEach(() => { i--; });
+
+                _suite.afterAll(() => { i--; });
+            });
+
+            var agent = new Agent();
+            agent.RunSuite(_suite);
+            i.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void agent_executesASpecAndRunsSuccesfullyWithHooksNestedDescribes()
+        {
+            var i = 0;
+            _suite.describe("new", () =>
+            {
+                _suite.describe("Nested1", () =>
+                {
+                    _suite.beforeAll(() => { i++; });
+
+                    _suite.beforeEach(() => { i++; });
+
+                    _suite.it("spec1", () =>
+                    {
+                        //
+                    });
+
+                    _suite.afterEach(() => { i--; });
+
+                    _suite.afterAll(() => { i--; });
+                });
+                _suite.describe("Nested2", () =>
+                {
+                    _suite.beforeAll(() => { i++; });
+
+                    _suite.beforeEach(() => { i++; });
+
+                    _suite.it("spec1", () =>
+                    {
+                        //
+                    });
+
+                    _suite.afterEach(() => { i--; });
+
+                    _suite.afterAll(() => { i--; });
+                });
+            });
+
+            var agent = new Agent();
+            agent.RunSuite(_suite);
+            i.Should().Be(0);
+        }
+
+        //[TestMethod] // Dynamic specs have some issues
+        public void suite_shouldAddDYnamicSpecs()
+        {
+            var dynamicSpecCount = 10;
+            _suite.describe("test describe", () =>
+            {
+                for (int i = 0; i < dynamicSpecCount; i++)
+                {
+                    _suite.it("the int " + i, () => { Console.Write(i); });
+                }
+            });
+
+            _suite.Registry.ExecutableLookupTable.Count.Should().Be(dynamicSpecCount);
+        }
+
+        [TestMethod]
+        public void agent_executes()
+        {
+            var i = new AllHookDouble();
+            var agent = new Agent();
+            agent.RunSuite(i);
+            //agent.RunSuite(i.Registry.CurrentSuite);
+        }
     }
-
-    [TestMethod]
-    public void agent_executes()
-    {
-      var i = new AllHookDouble();
-      var agent = new Agent();
-      agent.RunSuite(i);
-      //agent.RunSuite(i.Registry.CurrentSuite);
-
-    }     
-  }
 
     public class EachHookDouble : Spec
     {
@@ -253,17 +254,11 @@ namespace spec.tests
             describe("Init", () =>
             {
                 var i = 0;
-                beforeEach("mainforeach 0", () =>
-                {
-                    i = 0;
-                });
-                beforeEach("mainforeach 1", () =>
-                {
-                    i = 1;
-                });
+                beforeEach("mainforeach 0", () => { i = 0; });
+                beforeEach("mainforeach 1", () => { i = 1; });
                 it("1", () =>
                 {
-                    i = i+1;
+                    i = i + 1;
                     Console.WriteLine(i);
                     i.Should().Be(2);
                 });
@@ -275,54 +270,45 @@ namespace spec.tests
                 describe("xx", () =>
                 {
                     var j = 0;
-                    beforeEach("nested foreach",() =>
-                    {
-                        j = 1;
-                    });
+                    beforeEach("nested foreach", () => { j = 1; });
                     it("3", () =>
                     {
                         i.Should().Be(1);
                         j = j + 1;
                         j.Should().Be(2);
                     });
-                    afterEach("nested aftereach", () =>
-                    {
-                        
-                    });
+                    afterEach("nested aftereach", () => { });
                 });
-                afterEach("main aftereach", () =>
-                {
-
-                });
+                afterEach("main aftereach", () => { });
             });
         }
     }
 
-  public class AllHookDouble : Spec
-  {
-    public AllHookDouble()
+    public class AllHookDouble : Spec
     {
-        describe("level 1", () =>
+        public AllHookDouble()
         {
-            beforeAll("Main BeforeAll",() => { Console.WriteLine("L1 - before all"); });
-            beforeEach("Main Beforeach",() => { Console.WriteLine("  L1 - beforeEach"); });
-            afterEach(() => { Console.WriteLine("  L1 - afterEach"); });
-            afterAll("Main AfterALl",() => { Console.WriteLine("L1 - after all"); });
-
-            it("L1 test A", () => { Console.WriteLine("    L1 - test A"); });
-            it("L1 test B", () => { Console.WriteLine("    L1 - Test B"); });
-
-            describe("level 2", () =>
+            describe("level 1", () =>
             {
-                beforeAll("nested before all",() => { Console.WriteLine("    L2 - before all"); });
-                beforeEach("nested beforeach",() => { Console.WriteLine("      L2 - beforeEach"); });
-                afterEach("nested after each",() => { Console.WriteLine("      L2 - afterEach"); });
-                afterAll("nested after all",() => { Console.WriteLine("    L2 - after all"); });
+                beforeAll("Main BeforeAll", () => { Console.WriteLine("L1 - before all"); });
+                beforeEach("Main Beforeach", () => { Console.WriteLine("  L1 - beforeEach"); });
+                afterEach(() => { Console.WriteLine("  L1 - afterEach"); });
+                afterAll("Main AfterALl", () => { Console.WriteLine("L1 - after all"); });
 
-                it("L2 test A", () => { Console.WriteLine("        L2 - test A"); });
-                it("L2 test B", () => { Console.WriteLine("        L2 - test B"); });
+                it("L1 test A", () => { Console.WriteLine("    L1 - test A"); });
+                it("L1 test B", () => { Console.WriteLine("    L1 - Test B"); });
+
+                describe("level 2", () =>
+                {
+                    beforeAll("nested before all", () => { Console.WriteLine("    L2 - before all"); });
+                    beforeEach("nested beforeach", () => { Console.WriteLine("      L2 - beforeEach"); });
+                    afterEach("nested after each", () => { Console.WriteLine("      L2 - afterEach"); });
+                    afterAll("nested after all", () => { Console.WriteLine("    L2 - after all"); });
+
+                    it("L2 test A", () => { Console.WriteLine("        L2 - test A"); });
+                    it("L2 test B", () => { Console.WriteLine("        L2 - test B"); });
+                });
             });
-    });
+        }
     }
-  }
 }
