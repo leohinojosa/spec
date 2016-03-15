deec9afac09491af532841f5efde06c07db1d9f1
leohinojosa <leonardh@gmail.com>
2016-03-15 15:26:10 -0700

    fixing unit tests that had a hardcoded path

--- src/spec.tests/DiscoveryTest.cs
+++ src/spec.tests/DiscoveryTest.cs
@@ -11,8 +11,14 @@ namespace spec.tests
     [TestMethod]
     public void Discover_LoadSpecs()
     {
-      var sampleDll = @"..\..\..\SampleSpecs\bin\debug\SampleSpecs.dll";
-      using (var sandbox = new Sandbox<Discover>(sampleDll))
+        string sampleDll = "";
+#if DEBUG
+            sampleDll = @"..\..\..\SampleSpecs\bin\debug\SampleSpecs.dll";
+#else
+            sampleDll = @"..\..\..\SampleSpecs\bin\Release\SampleSpecs.dll";
+#endif
+
+            using (var sandbox = new Sandbox<Discover>(sampleDll))
       {
         var discoveredDefinitions = sandbox.Content.DiscoverSpecsFromCurrentAssembly();
         discoveredDefinitions.Should().NotBeEmpty("Because its loading spec definitions");
